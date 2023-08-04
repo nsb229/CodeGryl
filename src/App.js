61176498f4698f6aa2515e1521eacecb0b1a6955
Naina <ns1388@scarletmail.rutgers.edu>
2023-08-04 16:15:29 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.css";
+import Home from "./pages/Home";
+import { Routes, Route, useLocation } from "react-router-dom";
+import Cuisines from "./pages/Cuisines";
+import Category from "./components/Category";
+import Search from "./components/Search";
+import SingleRecipe from "./pages/SingleRecipe";
+import RecipeDetails from "./pages/RecipeDetails";
+import Navbar from "./components/Navbar";
+import { AnimatePresence } from "framer-motion";
 
 function App() {
+  const location = useLocation();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <Search />
+      <Category />
+      <AnimatePresence mode="wait">
+        <Routes location={location} key={location.pathname}>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/cuisine/:type" element={<Cuisines />}></Route>
+          <Route path="/recipe/:search" element={<SingleRecipe />}></Route>
+          <Route
+            path="/recipeDetails/:name"
+            element={<RecipeDetails />}
+          ></Route>
+        </Routes>
+      </AnimatePresence>
+    </>
   );
 }
 
